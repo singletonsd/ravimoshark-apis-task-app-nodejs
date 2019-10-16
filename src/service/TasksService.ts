5ebc091cd1041b5687680410e4cbca42de4fb407
dev <dev@ravimoshark.com>
2019-10-16 12:33:21 +0200

    Added get requests.

--- src/service/TasksService.ts
+++ src/service/TasksService.ts
@@ -1,6 +1,12 @@
 "use strict";
 
+import { getConnection } from "typeorm";
+import { Tasks } from "../databases/entities";
+import { DatabaseUtilities } from "../databases/utils/DatabaseUtils";
+import { Metadata } from "../models";
+import { LoggerUtility } from "../utils/LoggerUtility";
 import { ParametersComplete, ParametersIdDeleted, Utilities } from "../utils/utilities";
+import { VALID_RESPONSES } from "../utils/ValidResponses";
 
 const SERVICE_NAME = "TasksService";
 export class TasksService {
@@ -67,14 +73,22 @@ export class TasksService {
    * deleted Deleted Get all, deleted, not deleted data. Default not deleted. (optional)
    * returns Tasks
    */
-  public static getById(params: ParametersIdDeleted) {
-    return new Promise((resolve, reject) => {
-      const examples = {};
-      if (Object.keys(examples).length > 0) {
-        resolve(examples[Object.keys(examples)[0]]);
-      } else {
-        resolve();
-      }
+  public static getById(params: ParametersIdDeleted): Promise<Tasks> {
+    const FUNCTION_NAME = "getById";
+    const logHeader = `${SERVICE_NAME}: ${FUNCTION_NAME} -`;
+    return new Promise<Tasks>(async (resolve, reject) => {
+    LoggerUtility.info(`${logHeader}`);
+    LoggerUtility.debug(`${logHeader} with`, params);
+    const previous: Tasks = await getConnection().manager.findOne(Tasks,
+      DatabaseUtilities.getFindOneObject(params.id, params.deleted, Tasks));
+    if (!previous) {
+      LoggerUtility.warn(`${logHeader} not exists with id=${params.id} and deleted=${params.deleted.toString()}`);
+      reject(VALID_RESPONSES.ERROR.NOT_EXIST.TASK);
+      return;
+    }
+    LoggerUtility.info(`${logHeader} got ${previous.id}`);
+    resolve(previous);
+    return;
     });
   }
 
@@ -91,14 +105,27 @@ export class TasksService {
    * refClient String Data from a desired contract (optional)
    * returns inline_response_200
    */
-  public static get(params: ParametersComplete) {
-    return new Promise((resolve, reject) => {
-      const examples = {};
-      if (Object.keys(examples).length > 0) {
-        resolve(examples[Object.keys(examples)[0]]);
-      } else {
-        resolve();
+  public static get(params: ParametersComplete): Promise<{metadata: Metadata, items: Array<Tasks>}>  {
+    const FUNCTION_NAME = "get";
+    const logHeader = `${SERVICE_NAME}: ${FUNCTION_NAME} -`;
+    return new Promise(async (resolve, reject) => {
+      LoggerUtility.info(`${logHeader}`);
+      const object = DatabaseUtilities.getFindObject(params, Tasks);
+      if (!object) {
+          LoggerUtility.warn(`${logHeader} order param malformed`, params.orderBy);
+          reject(VALID_RESPONSES.ERROR.PARAMS.MALFORMED.ORDERBY);
+          return;
       }
+      LoggerUtility.debug(`${logHeader} with`, object);
+      const [items, total] = await getConnection().manager.findAndCount(Tasks, object);
+      if (!items || !items.length) {
+          LoggerUtility.warn(`${logHeader} empty result`);
+          resolve();
+          return;
+      }
+      LoggerUtility.info(`${logHeader} got ${items.length}`);
+      resolve(Utilities.getMetadataFormat(items, total, params));
+      return;
     });
   }
 
--- src/service/VisitsService.ts
+++ src/service/VisitsService.ts
@@ -1,6 +1,12 @@
 "use strict";
 
+import { getConnection } from "typeorm";
+import { Visits } from "../databases/entities";
+import { DatabaseUtilities } from "../databases/utils/DatabaseUtils";
+import { Metadata } from "../models";
+import { LoggerUtility } from "../utils/LoggerUtility";
 import { ParametersComplete, ParametersIdDeleted, Utilities } from "../utils/utilities";
+import { VALID_RESPONSES } from "../utils/ValidResponses";
 
 const SERVICE_NAME = "VisitsService";
 export class VisitsService {
@@ -67,14 +73,22 @@ export class VisitsService {
    * deleted Deleted Get all, deleted, not deleted data. Default not deleted. (optional)
    * returns Visits
    */
-  public static getById(params: ParametersIdDeleted) {
-    return new Promise((resolve, reject) => {
-      const examples = {};
-      if (Object.keys(examples).length > 0) {
-        resolve(examples[Object.keys(examples)[0]]);
-      } else {
-        resolve();
-      }
+  public static getById(params: ParametersIdDeleted): Promise<Visits> {
+    const FUNCTION_NAME = "getById";
+    const logHeader = `${SERVICE_NAME}: ${FUNCTION_NAME} -`;
+    return new Promise<Visits>(async (resolve, reject) => {
+    LoggerUtility.info(`${logHeader}`);
+    LoggerUtility.debug(`${logHeader} with`, params);
+    const previous: Visits = await getConnection().manager.findOne(Visits,
+        DatabaseUtilities.getFindOneObject(params.id, params.deleted, Visits));
+    if (!previous) {
+        LoggerUtility.warn(`${logHeader} not exists with id=${params.id} and deleted=${params.deleted.toString()}`);
+        reject(VALID_RESPONSES.ERROR.NOT_EXIST.VISIT);
+        return;
+    }
+    LoggerUtility.info(`${logHeader} got ${previous.id}`);
+    resolve(previous);
+    return;
     });
   }
 
@@ -91,14 +105,27 @@ export class VisitsService {
    * refClient String Data from a desired contract (optional)
    * returns inline_response_200_2
    */
-  public static get(params: ParametersComplete) {
-    return new Promise((resolve, reject) => {
-      const examples = {};
-      if (Object.keys(examples).length > 0) {
-        resolve(examples[Object.keys(examples)[0]]);
-      } else {
-        resolve();
+  public static get(params: ParametersComplete): Promise<{metadata: Metadata, items: Array<Visits>}>  {
+    const FUNCTION_NAME = "get";
+    const logHeader = `${SERVICE_NAME}: ${FUNCTION_NAME} -`;
+    return new Promise(async (resolve, reject) => {
+      LoggerUtility.info(`${logHeader}`);
+      const object = DatabaseUtilities.getFindObject(params, Visits);
+      if (!object) {
+          LoggerUtility.warn(`${logHeader} order param malformed`, params.orderBy);
+          reject(VALID_RESPONSES.ERROR.PARAMS.MALFORMED.ORDERBY);
+          return;
       }
+      LoggerUtility.debug(`${logHeader} with`, object);
+      const [items, total] = await getConnection().manager.findAndCount(Visits, object);
+      if (!items || !items.length) {
+          LoggerUtility.warn(`${logHeader} empty result`);
+          resolve();
+          return;
+      }
+      LoggerUtility.info(`${logHeader} got ${items.length}`);
+      resolve(Utilities.getMetadataFormat(items, total, params));
+      return;
     });
   }
 
